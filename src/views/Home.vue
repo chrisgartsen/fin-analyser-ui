502376e033f264e9d088ecc39a0e6eddaab1c4e7
Chris Gartsen <chris@gartsen.nl>
2019-08-01 19:30:10 +0200

    Add  navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+
+        <div class="field">
+            <b-checkbox v-model="checkboxCustom"
+                true-value="Yes"
+                false-value="No">
+                {{ checkboxCustom }}
+            </b-checkbox>
+        </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
